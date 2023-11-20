ee603d772dcd5392583c922154221994055f614a
nobel.naimul.riseuplabs <nobel.naimul.riseuplabs@gmail.com>
2023-11-20 17:04:18 +0600

    http server create

--- http-server/index.js
+++ http-server/index.js
@@ -1,8 +1,27 @@
 const http = require('http');
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Hello World!' }));
-});
+const server = http.createServer();
+server.on('request', (req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
+            message: 'Hello World!'
+        }));
+    } else if (req.url === '/about') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<html>
+        <head>
+          <title>About</title>
+          </head>
+          <body>
+            <h1>About Page</h1>
+            <p>This is the about page</p>
+            </body>
+            </html>`);
+        res.end();
+    }
+})
 server.listen(8000, () => {
     console.log('Server is running on port 8000...');
 })
