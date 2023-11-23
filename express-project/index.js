2fdf81e9d11041eb77b6131f5609780080fb1f89
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2023-11-23 16:41:43 +0600

    rendering hbs template

--- express-project/index.js
+++ express-project/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+
 const port = 3000;
 const messagesRouter = require('./routes/messages.router');
 const friendsRouter = require('./routes/friends.router');
 const path = require('path');
-app.use("/", express.static(path.join(__dirname, 'public')));
+app.use("/static", express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 app.use((req, res, next) => {
     const start = Date.now();
     next();
@@ -16,6 +18,12 @@ app.use((req, res, next) => {
 
 app.use('/messages', messagesRouter);
 app.use('/friends', friendsRouter);
+app.use('/', (req, res) => {
+    res.render('index', {
+        title: 'Hello World!',
+        message: 'Hello there!'
+    })
+})
 app.listen(port, () => {
     return console.log(`Server is running on port ${port}...`);
 })
