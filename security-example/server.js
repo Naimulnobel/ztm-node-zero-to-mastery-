3e74a21568b3a216e07b50de32d60643a59115f5
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2024-01-02 17:15:08 +0600

    security example

---
+++ security-example/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const PORT = 3000;
+const app = express();
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+}
+);
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+})
