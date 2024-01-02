180ba2664e451b68ba6d09ae84f9df48a94eef02
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2024-01-02 17:20:29 +0600

    Update server.js

--- security-example/server.js
+++ security-example/server.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const PORT = 3000;
 const app = express();
+app.get('/secret', (req, res) => {
+    res.send('your personal secret value is 42');
+})
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 }
