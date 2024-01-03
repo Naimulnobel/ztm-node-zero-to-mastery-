9b2cb51f8f0e8e8c5d33f6994a7e176d5f3063d9
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2024-01-03 16:16:21 +0600

    security enhanced by helmet js

--- security-example/server.js
+++ security-example/server.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
 const path = require('path');
+const https = require('https');
 const express = require('express');
+const helmet = require('helmet');
+
 const PORT = 3000;
 const app = express();
+app.use(helmet());
 app.get('/secret', (req, res) => {
     res.send('your personal secret value is 42');
 })
@@ -9,6 +14,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 }
 );
-app.listen(PORT, () => {
+https.createServer(
+    {
+        key: fs.readFileSync('key.pem'),
+        cert: fs.readFileSync('cert.pem')
+
+    }, app
+).listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
 })
