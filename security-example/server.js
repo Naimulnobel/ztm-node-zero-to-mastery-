0f9415bf8798dbef5f7587d62ab5028f093532f9
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2024-01-10 11:17:01 +0600

    security and oauth integration

--- security-example/server.js
+++ security-example/server.js
@@ -3,17 +3,40 @@ const path = require('path');
 const https = require('https');
 const express = require('express');
 const helmet = require('helmet');
-
+require('dotenv').config();
 const PORT = 3000;
 const app = express();
 app.use(helmet());
-app.get('/secret', (req, res) => {
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+}
+function checkLoggedIN(req, res, next) {
+    const isLoggedIn = true;
+    if (!isLoggedIn) {
+        res.status(401).json({
+            error: 'you must be logged in to access this resource'
+        })
+    }
+    next();
+}
+app.get('/auth/google', (req, res) => {
+
+});
+app.get('/auth/google/callback', (req, res) => {
+
+});
+app.get('/auth/logout', (req, res) => {
+
+});
+app.get('/secret', checkLoggedIN, (req, res) => {
     res.send('your personal secret value is 42');
 })
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 }
 );
+
 https.createServer(
     {
         key: fs.readFileSync('key.pem'),
