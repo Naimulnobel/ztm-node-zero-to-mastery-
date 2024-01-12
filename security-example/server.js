201c1781f540ddcb368268770c81b2a932ed4221
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2024-01-12 18:03:21 +0600

    update in oauth

--- security-example/server.js
+++ security-example/server.js
@@ -5,14 +5,22 @@ const express = require('express');
 const helmet = require('helmet');
 require('dotenv').config();
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 const { Strategy } = require('passport-google-oauth20');
 const PORT = 3000;
 const app = express();
 app.use(helmet());
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+}))
 app.use(passport.initialize());
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2
 }
 passport.use(new Strategy({
     clientID: config.CLIENT_ID,
